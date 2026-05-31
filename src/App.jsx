4dd1302390a2cc90bb9f3dda5f5729133233a0c1
suchir7 <suchirpitti@gmail.com>
2026-05-31 16:39:16 +0530

    Updation of API_URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://ragbot-api-xczz.onrender.com";
 
 export default function App() {
   const [messages, setMessages] = useState([
